1511316749e830d410e0aee0d492c8876d43f9ca
Duane Wellington <duanewellington@Duanes-MacBook-Pro.local>
2023-10-24 18:51:17 -0400

    task 3

--- app.js
+++ app.js
@@ -1,11 +1,66 @@
 console.log('JS:Loaded')
 
+// Menu data structure
+const menuLinks = [
+    {text: 'about', href: '/about'},
+    {text: 'catalog', href: '/catalog'},
+    {text: 'orders', href: '/orders'},
+    {text: 'account', href: '/account'},
+  ];
+
+
+//   const words = ["one", "two", "three", "four"];
+//   words.forEach((word) => {
+//     console.log(word);
+//     if (word === "two") {
+//       words.shift(); //'one' will delete from array
+//     }
+//   }); // one // two // four
+  
+//   console.log(words); // ['two', 'three', 'four']
+
+
+// Task 1
+// .0
 const mainEl = document.querySelector("main")
 
-// console.log(mainEl)
+// .1
 mainEl.style.backgroundColor = 'var(--main-bg)'
-mainEl.innerHTML = '<h1>SEI Rocks!</h1>'
-//mainEl.setAttribute('class', 'flex-ctr')
 
+// .2
+mainEl.innerHTML = '<h1>SEI Rocks!</h1>'
+
+// .3
 mainEl.classList.add("flex-ctr")
-console.dir(mainEl)
+
+// ----------------
+
+// Task 2
+// .0
+const topMenuEl = document.getElementById('top-menu')
+//console.log(topMenuEl)
+
+// .1
+topMenuEl.style.height = '100%'
+
+// .2
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)'
+
+// .3
+topMenuEl.classList.add('flex-around')
+
+// -----------------
+
+// Task 3
+
+// .1
+
+   menuLinks.forEach((link) => {
+    document.createElement('a')
+    return link
+})
+   const hrefEl = document.createAttribute('a', href)
+
+//console.log(link)
+
+
