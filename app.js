abe821e029c152965669c911d8474df8be39bd52
Duane Wellington <duanewellington@Duanes-MacBook-Pro.local>
2023-10-24 16:09:44 -0400

    task 1.2

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-mainEl = document.querySelector('main')
-console.log('main')
+console.log('JS:Loaded')
+
+const mainEl = document.querySelector("main")
+
+// console.log(mainEl)
+mainEl.style.backgroundColor = 'var(--main-bg)'
+mainEl.innerHTML = '<h1>SEI Rocks!</h1>'
+//mainEl.setAttribute('class', 'flex-ctr')
+
+mainEl.classList.add("flex-ctr")
+console.dir(mainEl)
